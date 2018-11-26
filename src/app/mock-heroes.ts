4d76542398b9b52cc2f2da06fbc6b99b1b4fb450
azg183 <garza1324@gmail.com>
2018-11-26 12:38:21 -0500

    Part 1 of Tutorial

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,16 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero [] = [
+ 
+    { id: 11, name: 'Mr. Nice', occupation: 'Developer', age: 25 },
+    { id: 12, name: 'Narco', occupation: 'Developer', age: 26 },
+    { id: 13, name: 'Bombasto', occupation: 'Developer', age: 46 },
+    { id: 14, name: 'Celeritas', occupation: 'Manager', age: 33 },
+    { id: 15, name: 'Magneta',  occupation: 'Developer', age: 21 },
+    { id: 16, name: 'RubberMan', occupation: 'Developer', age: 53 },
+    { id: 17, name: 'Dynama', occupation: 'Developer', age: 75 },
+    { id: 18, name: 'Dr IQ', occupation: 'Manager', age: 32 },
+    { id: 19, name: 'Magma', occupation: 'Developer', age: 29 },
+    { id: 20, name: 'Tornado', occupation: 'Developer', age: 23 }
+
+];
